aab1de3140f01aceed8df5d26237b2e8e4a443b2
ShamimHossain1 <beastb697@gmail.com>
2023-02-03 00:40:40 +0600

    slice

--- split-join.js
+++ split-join.js
@@ -1,6 +1,6 @@
 const paragraph = "Autism spectrum disorder (ASD) is a developmental, disability caused by differences in the brain, People with ASD often have problems with social communication and interaction, and restricted or repetitive behaviors or interests."
  // split
- 
+
 var parts = paragraph.split(" ");
 console.log(parts);
 
@@ -11,3 +11,12 @@ var chars = paragraph.split("");
 console.log(chars);
 
 
+// slice
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const partial = numbers.slice(3, 7);
+console.log(partial);
+
+const partial2 = paragraph.substring(4, 30);
+console.log(partial2);
