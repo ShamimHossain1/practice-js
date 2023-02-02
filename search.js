94c16d2434b4c67f588473624861d7fba754704b
ShamimHossain1 <beastb697@gmail.com>
2023-02-03 00:35:08 +0600

    split

--- search.js
+++ search.js
@@ -1,5 +1,14 @@
 const paragraph = "Autism spectrum disorder (ASD) is a developmental disability caused by differences in the brain. People with ASD often have problems with social communication and interaction, and restricted or repetitive behaviors or interests."
 
+
+// starts with
+
+console.log(paragraph.startsWith("Autism"));
+
+//Ends With
+
+console.log(paragraph.endsWith('behaviors'));
+
 const doesExist = paragraph.includes("developmental ");
 const doesntExist = paragraph.includes("Practice ");
 
@@ -19,9 +28,20 @@ console.log(exis);
 
 
 
-const numbers = '1 2 3 4 5 6 7 8 9';
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const searchNum = '5';
+const searchNum = 5;
 
 const exist = numbers.includes(searchNum);
 console.log(exist);
+
+if (numbers.indexOf(12) !== -1){
+    console.log('exist Inside the const');
+}
+else{
+    console.log('its not available');
+}
+
+
+
+
---
+++ split-join.js
@@ -0,0 +1,13 @@
+const paragraph = "Autism spectrum disorder (ASD) is a developmental, disability caused by differences in the brain, People with ASD often have problems with social communication and interaction, and restricted or repetitive behaviors or interests."
+ // split
+ 
+var parts = paragraph.split(" ");
+console.log(parts);
+
+var commma = paragraph.split(",");
+console.log(commma);
+
+var chars = paragraph.split("");
+console.log(chars);
+
+
