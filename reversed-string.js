6b7829fd1a55ada2089efeb7196ee94ff411936f
ShamimHossain1 <beastb697@gmail.com>
2023-02-06 14:08:07 +0600

    fibo number

--- reversed-string.js
+++ reversed-string.js
@@ -1,13 +1,16 @@
-function reversedString(text){
-   let reversed = '';
-    for ( let i= text.length-1; i>=0; i--){
+function reversedString(text) {
+    let reversed = '';
+    for (let i = text.length - 1; i >= 0; i--) {
         const element = text[i];
         reversed = reversed + element;
     }
-     return reversed;
+    return element;
 
 
 }
-
 var string = 'i am goku';
-console.log(reversedString(string));
+reversed = reversedString(string)
+console.log(reversed);
+
+
+
